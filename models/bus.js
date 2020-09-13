cc69fa745f6f84d5fa328fddb38370d0bf68fb67
Kshitij Gahoi <gahoikshitij7@gmail.com>
2020-09-13 16:30:44 +0530

    updated buses model

--- models/bus.js
+++ models/bus.js
@@ -3,7 +3,19 @@ const Schema = mongoose.Schema;
 
 let BusSchema = new Schema({
     name: {type: String, required: true, max: 100},
-    price: {type: Number, required: true},
+    registration_number: {type: String},
+    owner: {
+        name: {type: String},
+        phone: {type: Number}
+    },
+    driver: {
+        name: {type: String},
+        phone: {type: Number}
+    },
+    type: {type: String},
+    seat_type: {type: String},
+    available_seats: {type: Number},
+    total_seats: {type: Number}
 });
 
 
